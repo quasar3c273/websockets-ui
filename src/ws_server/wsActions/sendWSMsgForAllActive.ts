0ed7aef9f1d6918bf5a86c865d546906d4f38be8
Lapitskiy <hiimisha@mail.ru>
2024-03-01 11:12:11 +0300

    feat: msg for all

---
+++ src/ws_server/wsActions/sendWSMsgForAllActive.ts
@@ -0,0 +1,23 @@
+import {wsStore} from "~/store/wsStore";
+
+export const generalMessage = (type: string, data: string): void => {
+  try {
+    const response = {
+      type,
+      data,
+      id: 0,
+    }
+
+    for (const client of wsStore.clients) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(response));
+      }
+    }
+
+    console.log(`Msg for all: type: ${type}, value: ${data}`);
+  } catch (err) {
+    if (err) {
+      console.error(err.message);
+    }
+  }
+}
